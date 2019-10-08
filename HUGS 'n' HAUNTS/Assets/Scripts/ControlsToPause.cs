ab805f77dce805f9950ac94ab2bb0d74eb923aa8
Tara Langhorne <tara.langhorne@ufl.edu>
2019-10-08 17:01:11 -0400

    Buttons Fix 2
    Fixed some buttons

---
+++ HUGS 'n' HAUNTS/Assets/Scripts/ControlsToPause.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsToPause : MonoBehaviour
+{
+    ///pause menu variable
+    public GameObject pauseMenu;
+    public GameObject controlsMenu;
+
+
+    //On click
+    public void ControlsOpenPause()
+    {
+        //pause the game code here:
+
+        //open pause menu
+        controlsMenu.SetActive(false); //bring up pause menu
+        pauseMenu.SetActive(true); //turn off pause button so can't click again while in pause window
+    }
+}
