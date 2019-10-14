7a5b833e9e2cdfbab94fc31aa0ae45eacdec0f6f
Emmitta J Lewis <47674575+Animarts@users.noreply.github.com>
2019-10-14 02:24:22 -0400

    Add files via upload

---
+++ PauseScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScreen : MonoBehaviour
+{
+    //pause menu variable
+    public GameObject pauseMenu;
+    public GameObject pauseButton;
+
+    //On click
+    public void PauseMenuOpen()
+    {
+        //pause the game code here:
+
+        //open pause menu
+        pauseMenu.SetActive(true); //bring up pause menu
+        pauseButton.SetActive(false); //turn off pause button so can't click again while in pause window
+    }
+}
