062b201cca08069d1a0e10401d8f4975d9f22fd7
Tara Langhorne <tara.langhorne@ufl.edu>
2019-11-14 13:22:06 -0500

    Updated Version with Platform Position and Other Stuff

---
+++ HNH UNITY FILES-11-14-19/Assets/Scripts/CollectibeHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibeHealth : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject health;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            player.GetComponent<SpooksterHealth>().AddHealth();
+            Destroy(health);
+        }
+    }
+}
