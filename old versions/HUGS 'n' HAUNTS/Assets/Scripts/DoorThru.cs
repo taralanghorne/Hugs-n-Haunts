f1078a000042257bb8fc10e6f156867ac7238fca
Tara Langhorne <tara.langhorne@ufl.edu>
2019-11-21 15:44:51 -0500

    New Version Updated

---
+++ old versions/HUGS 'n' HAUNTS/Assets/Scripts/DoorThru.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorThru : MonoBehaviour
+{
+    public GameObject wall;
+    public GameObject orbText;
+    //public Transform mainCamera;
+    public AudioClip door;
+    public GameObject passThrough;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        wall.SetActive(false);
+        orbText.SetActive(false);
+        passThrough.SetActive(false);
+        AudioSource.PlayClipAtPoint(door, transform.position);
+    }
+}
---
+++ old versions/HUGS 'n' HAUNTS/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public void OpenMainMenu()
+    {
+        SpooksterHealth.lives = 5;
+        SpooksterHealth.lose = false;
+        PlayerScoreSpookster.playerscore = 0; // reset Score
+        PauseScreenPressP.isPaused = false;
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+}
